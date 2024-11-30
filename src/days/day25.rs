68fba01d400d3a2767ac1a6f879d675eb4ee11b7
arthomnix <arthomnix@arthomnix.dev>
2024-11-30 22:14:23 +0000

    Initial commit

---
+++ src/days/day25.rs
@@ -0,0 +1,7 @@
+pub(crate) fn part1(input: String) {
+    unimplemented!("This puzzle has not been completed yet!");
+}
+
+pub(crate) fn part2(input: String) {
+    unimplemented!("Advent of Code puzzles do not have a part 2 on Christmas Day!");
+}
---
+++ src/days/mod.rs
@@ -0,0 +1,27 @@
+#![allow(unused_variables)]
+
+pub(crate) mod day1;
+pub(crate) mod day2;
+pub(crate) mod day3;
+pub(crate) mod day4;
+pub(crate) mod day5;
+pub(crate) mod day6;
+pub(crate) mod day7;
+pub(crate) mod day8;
+pub(crate) mod day9;
+pub(crate) mod day10;
+pub(crate) mod day11;
+pub(crate) mod day12;
+pub(crate) mod day13;
+pub(crate) mod day14;
+pub(crate) mod day15;
+pub(crate) mod day16;
+pub(crate) mod day17;
+pub(crate) mod day18;
+pub(crate) mod day19;
+pub(crate) mod day20;
+pub(crate) mod day21;
+pub(crate) mod day22;
+pub(crate) mod day23;
+pub(crate) mod day24;
+pub(crate) mod day25;
