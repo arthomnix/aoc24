18cdea4672ce7506f12673be179be53a51c18fda
arthomnix <arthomnix@arthomnix.dev>
2024-12-02 07:01:09 +0000

    Day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,7 +1,41 @@
+#[inline(always)]
+pub(crate) fn day(input: String, part2: bool) {
+    let count = input
+        .lines()
+        .map(|line| {
+            let split: Vec<i32> = line
+                .split_whitespace()
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect();
+
+            // dirty, but still very fast on input
+            let mut split_v = vec![split.clone()];
+            if part2 {
+                for i in 0..split.len() {
+                    let mut v = split.clone();
+                    v.remove(i);
+                    split_v.push(v);
+                }
+            }
+
+            if split_v.iter().any(|split| {
+                let diffs: Vec<i32> = split.windows(2)
+                    .map(|w| w[1] - w[0])
+                    .collect();
+
+                let is_safe_increasing = diffs.iter().all(|&x| x >= 1 && x <= 3);
+                let is_safe_decreasing = diffs.iter().all(|&x| x <= -1 && x >= -3);
+                is_safe_increasing || is_safe_decreasing
+            }) { 1 } else { 0 }
+
+        }).sum::<i32>();
+    println!("{count}");
+}
+
 pub(crate) fn part1(input: String) {
-    unimplemented!("This puzzle has not been completed yet!");
+    day(input, false);
 }
 
 pub(crate) fn part2(input: String) {
-    unimplemented!("This puzzle has not been completed yet!");
+    day(input, true);
 }
