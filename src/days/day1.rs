a3a9419068cad67b12d09e182fe6093293427016
arthomnix <arthomnix@arthomnix.dev>
2024-12-23 08:24:52 +0000

    day1 p1 avx512

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,22 +1,32 @@
+use std::arch::x86_64::*;
 use std::collections::HashMap;
 use std::str::FromStr;
 
-pub(crate) fn part1(input: String) {
-    let mut left = vec![];
-    let mut right = vec![];
+#[repr(align(64))]
+struct Aligned([i32; 1008]);
 
-    for line in input.lines() {
-        let mut split = line.split_whitespace();
-        left.push(i32::from_str(split.next().unwrap()).unwrap());
-        right.push(i32::from_str(split.next().unwrap()).unwrap());
+pub(crate) fn part1(input: String) {
+    let mut left = Aligned([0; 1008]);
+    let mut right = Aligned([0; 1008]);
+
+    for (i, line) in input.lines().enumerate() {
+        left.0[i] = line[..5].parse().unwrap();
+        right.0[i] = line[8..].parse().unwrap();
     }
 
-    left.sort_unstable();
-    right.sort_unstable();
+    left.0.sort_unstable();
+    right.0.sort_unstable();
 
     let mut total = 0;
-    for i in 0..left.len() {
-        total += (left[i] - right[i]).abs();
+    for i in 0..63 {
+        let j = i << 4;
+        unsafe {
+            let mut ll = _mm512_load_epi32(&left.0[j] as *const i32);
+            let rl = _mm512_load_epi32(&right.0[j] as *const i32);
+            ll = _mm512_sub_epi32(ll, rl);
+            ll = _mm512_abs_epi32(ll);
+            total += _mm512_reduce_add_epi32(ll);
+        }
     }
     println!("{total}");
 }
